e4fc24888c0183b3d62299d4b5d51cbddae034df
felip <felipe77639@gmail.com>
2021-03-16 19:25:56 -0500

    interfaz

---
+++ Assets/Scripts/Principal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Principal : MonoBehaviour
+{
+    public GameObject Nombre;
+    public GameObject Direccion;
+    public GameObject DireccionPostal;
+    public GameObject  fechaFactura;
+    public GameObject Facturadesdehasta;
+    public GameObject DiaDefactura;
+    public GameObject ConsumoActiva;
+    public GameObject LecturaActual;
+    public GameObject LeccturaAnterior;
+    public GameObject FactorMultiplicacion;
+    public GameObject ConsumoReactiva;
+    public GameObject LecturaActualReactiva;
+    public GameObject LecturaAnteriorReactiva;
+    public GameObject FactorMultiplicacionReactiva;
+
+    public Dropdown Municipio;
+    public Dropdown Estrato;
+    
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
