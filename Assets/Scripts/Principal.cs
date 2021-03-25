3c18e19b362fffcdc38fbdfbabe99ce12f477281
felip <felipe77639@gmail.com>
2021-03-24 19:39:57 -0500

    fecha

--- Assets/Scripts/Principal.cs
+++ Assets/Scripts/Principal.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.DataTime;
+using System.Globalization;
+
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using System.DataTime;
 
 
 public class Principal : MonoBehaviour
@@ -28,17 +30,30 @@ public class Principal : MonoBehaviour
     public TMP_Dropdown Estrato;  
     public GameObject TotalKilowatios;
     public GameObject Subsidio;  
-    DateTime fecha;
-    
-
+    System.DateTime Desde;
+    System.DateTime Hasta;
+    System.TimeSpan Resultado;
+    System.String valor;
+    string Myformat;
+    string prueba;
 
     void Start()
     {   
-        DropdownItemSelected(Municipio);
-        Municipio.onValueChanged.AddListener(delegate{DropdownItemSelected(Municipio);});
-        DropdownItemSelected(Estrato);
-        Estrato.onValueChanged.AddListener(delegate{DropdownItemSelectedEstrato(Estrato);});
-        fecha = new DateTime("13/mar/2021");
+        // DropdownItemSelected(Municipio);
+        // Municipio.onValueChanged.AddListener(delegate{DropdownItemSelected(Municipio);});
+        // DropdownItemSelected(Estrato);
+        // Estrato.onValueChanged.AddListener(delegate{DropdownItemSelectedEstrato(Estrato);});
+        Myformat=@"ddMMyyyy";
+        prueba="12/ene/2021";
+        valor=@"12/ene/2021";
+        // Desde= new System.DateTime.TryParseExact(prueba,Myformat,CultureInfo.InvariantCulture);
+        Hasta=System.DateTime.ParseExact(valor,ParseClient.DateFormatString,System.Globalization.CultureInfo.InvariantCulture);
+           
+        // Hasta=new System.DateTime((long).ToDouble(prueba));
+        //  Desde=new System.DateTime(2014,11,16Convert);
+         
+        //  Resultado=Desde.Subtract(10);
+          print(Hasta.ToString());
     }
 
     
@@ -47,13 +62,13 @@ public class Principal : MonoBehaviour
         
     }
 
-    void DropdownItemSelected(TMP_Dropdown dropdown){
-        int index=dropdown.value;
+    // void DropdownItemSelected(TMP_Dropdown dropdown){
+    //     int index=dropdown.value;
 
-    }
+    // }
 
-    void DropdownItemSelectedEstrato(TMP_Dropdown dropdown){
-        int index=dropdown.value;
+    // void DropdownItemSelectedEstrato(TMP_Dropdown dropdown){
+    //     int index=dropdown.value;
         
-    }
+    // }
 }
